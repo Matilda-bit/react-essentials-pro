474b892e7bee24ec6c7cb6e9d4d46345fbb6e674
Matilda-bit <polinaovras@gmail.com>
2024-04-11 19:59:38 +0300

    added Components and data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './assets/react-core-concepts.png';
 import './App.css';
+import logo from './assets/react-core-concepts.png';
+import componentsLogo from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -23,11 +25,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header/>
       <main>
+        <section id='core-concepts'>
+          <h2>Core concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((data,index) => (
+              <CoreConcept 
+                key={index}
+                title={data.title} 
+                description={data.description} 
+                image={data.image} />
+            ))}
+            
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
