d4845e1d435426ca3f34fd5fad6e67e931c3c550
Matilda-bit <polinaovras@gmail.com>
2024-04-13 17:36:24 +0300

    added sections and tabs components

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,14 +1,18 @@
 import { CORE_CONCEPTS } from '../data.js';
 import CoreConcept from './CoreConcept.jsx';
+import Section from './Section.jsx';
 
 export default function CoreConcepts() {
     return  (
-    <section id='core-concepts'>
+        <section id='core-concepts'>
         <h2>Core concepts</h2>
-        <ul>
-        {CORE_CONCEPTS.map((data, index) => (
-            <CoreConcept key={index} {...data} />
-        ))}
-        </ul>
-    </section>);
+        {/* <Section id='core-concept' title='Core Concept'>         */}
+            <ul>
+            {CORE_CONCEPTS.map((data, index) => (
+                <CoreConcept key={index} {...data} />
+            ))}
+            </ul>
+        {/* </Section> */}
+        </section>
+    );
 }
